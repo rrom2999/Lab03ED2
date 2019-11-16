a406f22813dae997361b99e8044b70092066d0c0
Ronald Ovalle <rrom2999@gmail.com>
2019-11-16 00:13:25 -0600

    Clase Pizza

---
+++ Serie2-3/Clase/Pizza.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Serie2-3.Clase
+{
+    public class Pizza
+{
+}
+}
---
+++ Serie2y3/CLASES/Pizza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Serie2y3.CLASES
+{
+    public class Pizza
+    {
+        public string[] Ingredientes { get; set; }
+        public string Masa { get; set; }
+        public string Size { get; set; }
+        public int Porciones { get; set; }
+        public bool ExtraQueso { get; set; }
+    }
+}
